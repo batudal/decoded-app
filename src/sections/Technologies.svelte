e1ce2a382eef7a5ee85103d4d86d6929056a67cb
silvijasaule <s.aleknaite@gmail.com>
2022-10-25 10:39:32 +0300

    Don't show rocket on smaller screen

--- src/sections/Technologies.svelte
+++ src/sections/Technologies.svelte
@@ -1,9 +1,7 @@
 <main>
 	<img src="/images/rocket.png" alt="rocket" />
-	<div style="width:64px" />
 	<div class="content">
 		<h2>Technologies we love and use</h2>
-		<div style="height:32px" />
 		<div class="items">
 			{#each Array(18) as _, i}
 				<img src={`svg/tech-${i + 1}.svg`} alt="" />
@@ -23,13 +21,34 @@
 	}
 	main > img {
 		height: 191px;
+		margin-right: 64px;
 	}
 	.content {
-		width: 540px;
+		width: 67.5%;
+	}
+	
+	h2 {
+		margin-bottom: 32px;
 	}
 	.items {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap;
+	}
+	@media (max-width:810px) {
+		.content {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+		main > img {
+			display: none;
+		}
+		.content {
+			width: 100%;
+		}
+	}
+	@media (max-width:567px) {
 	}
 </style>
