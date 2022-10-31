86e5079bd6d5db256c712caa1e71a2ed7804621d
silvijasaule <s.aleknaite@gmail.com>
2022-10-31 10:54:15 +0200

    Responsive features section

--- src/sections/Features.svelte
+++ src/sections/Features.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import GradientCard from '../components/GradientCard.svelte';
+	import MobileGradientCard from '../components/MobileGradientCard.svelte';
 
 	let innerWidth = 0;
 </script>
@@ -7,24 +8,48 @@
 <svelte:window bind:innerWidth />
 <h4 class="gradient-text">NEVERMIND THE OBSTACLES</h4>
 <main>
+	{#if innerWidth > 567}
 	<GradientCard
 		layout="single"
 		title="Specialized Partner Network"
 		desc="Bring your business model to life entrusting us with the technical aspects.We provide cutting edge user experience to your clients both on and off the chain with our vast experience, understanding of user pain points  and market know-how. "
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-1.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+		title="Specialized Partner Network"
+		desc="Bring your business model to life entrusting us with the technical aspects.We provide cutting edge user experience to your clients both on and off the chain with our vast experience, understanding of user pain points  and market know-how. "
+		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-1.webp"
+	/>
+	{/if}
+	{#if innerWidth > 567}
 	<GradientCard
 		layout={innerWidth > 1000 ? "double" : "single"}
 		title="Longterm Support"
 		desc="Get competitive advantage in  the fast paced market by incorporating leading practices in your token economy."
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-2.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+		title="Longterm Support"
+		desc="Get competitive advantage in  the fast paced market by incorporating leading practices in your token economy."
+		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-2.webp"
+	/>
+	{/if}
+	{#if innerWidth > 567}
 	<GradientCard
 		layout={innerWidth > 1000 ? "double" : "single"}
 		title="Quality Assurance"
 		desc="Make long lasting and intelligent choices when building your strategy with the technology guidance from experienced crypto-native network."
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-3.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+		title="Quality Assurance"
+		desc="Make long lasting and intelligent choices when building your strategy with the technology guidance from experienced crypto-native network."
+		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/obs-3.webp"
+	/>
+	{/if}
 </main>
 
 <style>
