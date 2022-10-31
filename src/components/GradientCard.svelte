b2999de4a5bfd23e9a0209d6f46dc0001182f6a0
silvijasaule <s.aleknaite@gmail.com>
2022-10-31 10:49:23 +0200

    Mobile gradient card

--- src/components/GradientCard.svelte
+++ src/components/GradientCard.svelte
@@ -61,44 +61,5 @@
 		margin: 0;
 	}
 
-	@media (max-width:567px) {
-		main {
-			/* height: 466px; */
-			display: flex;
-			flex-direction: row;
-			flex-wrap: wrap;
-			background: none;
-			align-items:center;
-			justify-content: center;
-			gap:24px;
-		}
-		.img-container {
-			height: 200px;
-			width: 300px;
-			background: linear-gradient(94.7deg, rgba(255, 255, 255, 0.07) 1.77%, rgba(255, 255, 255, 0) 100%);
-			border: 1px solid rgba(255, 255, 255, 0.2);
-			border-radius: 12px;
-			backdrop-filter: blur(2px);
-		}
-		.single-img,
-		.double-img {
-			margin: 0;
-			height: 200px;
-		}
-		.single > .content,
-		.double > .content {
-			margin: 0;
-			max-width: 276px;
-		}
 
-		h2 {
-			font-size: 32px;
-			line-height: 32px;
-		}
-		p
-		{
-			font-size: 16px;
-			line-height: 22px;
-		}
-	}
 </style>
--- src/sections/StrategicNeeds.svelte
+++ src/sections/StrategicNeeds.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import GradientCard from '../components/GradientCard.svelte';
+	import MobileGradientCard from '../components/MobileGradientCard.svelte';
 	
 	let innerWidth = 0;
 </script>
@@ -8,24 +9,49 @@
 
 <h4 id="services" class="gradient-text">WE’LL COVER YOUR STRATEGIC NEEDS</h4>
 <main>
+	{#if innerWidth > 567}
 	<GradientCard
 		layout="single"
 		title="Decentralized App Development"
 		desc= "Bring your business model to life entrusting us with the technical aspects.We provide cutting edge user experience to your clients both on and off the chain with our vast experience, understanding of user pain points  and market know-how. "
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-1.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+		title="Decentralized App Development"
+		desc= "Bring your business model to life entrusting us with the technical aspects.We provide cutting edge user experience to your clients both on and off the chain with our vast experience, understanding of user pain points  and market know-how. "
+		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-1.webp"
+	/>
+	{/if}
+
+	{#if innerWidth > 567}
 	<GradientCard
 		layout= {innerWidth > 1017 ? "double" : "single"}
 		title="Security Audits"
 		desc="Get competitive advantage in  the fast paced market by incorporating leading practices in your token economy."
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-2.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+	title="Security Audits"
+	desc="Get competitive advantage in  the fast paced market by incorporating leading practices in your token economy."
+	image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-2.webp"
+	/>
+	{/if}
+	{#if innerWidth > 567}
 	<GradientCard
 		layout={innerWidth > 1017 ? "double" : "single"}
 		title="Private Network"
 		desc="Deploy your own app-chain or multi-party consensus system with features like chain explorer, bridge, oracles and more."
 		image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-3.webp"
 	/>
+	{:else}
+	<MobileGradientCard
+	title="Private Network"
+	desc="Deploy your own app-chain or multi-party consensus system with features like chain explorer, bridge, oracles and more."
+	image="https://decoded-files.fra1.cdn.digitaloceanspaces.com/web-assets/landing/service-3.webp"
+	/>
+	{/if}
 </main>
 
 <style>
